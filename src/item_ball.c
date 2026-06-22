758127fa9f94fd82378822c24755777829b2294d
FFZackFair92 <ashsolo92@gmail.com>
2026-06-22 13:03:26 +0200

    poke-ball Hoenn: +32 oggetti inutili (tesori/repel/fuga) nel pool per aumentare la difficolta - risorse vere piu rare

--- src/item_ball.c
+++ src/item_ball.c
@@ -38,12 +38,25 @@ void GetItemBallIdAndAmountFromTemplate(void)
 void NvGetBerryRandomItem(void)
 {
     static const u16 sPool[] = {
+        // --- UTILI alla run (28): cure / PP / vitamine / ball ---
         ITEM_POTION, ITEM_SUPER_POTION, ITEM_HYPER_POTION, ITEM_MAX_POTION, ITEM_FULL_RESTORE,
         ITEM_REVIVE, ITEM_MAX_REVIVE, ITEM_FULL_HEAL, ITEM_ANTIDOTE, ITEM_PARALYZE_HEAL,
         ITEM_AWAKENING, ITEM_BURN_HEAL, ITEM_ICE_HEAL, ITEM_ETHER, ITEM_MAX_ETHER,
         ITEM_ELIXIR, ITEM_MAX_ELIXIR, ITEM_PP_UP, ITEM_RARE_CANDY, ITEM_HP_UP,
         ITEM_PROTEIN, ITEM_IRON, ITEM_CALCIUM, ITEM_CARBOS, ITEM_ZINC,
         ITEM_GREAT_BALL, ITEM_ULTRA_BALL, ITEM_NUGGET,
+        // --- INUTILI alla run (difficolta'): tesori vendibili, repellenti (inutili col
+        //     toggle repel), oggetti di fuga. Diluiscono il pool -> risorse vere piu' rare. ---
+        ITEM_TINY_MUSHROOM, ITEM_BIG_MUSHROOM, ITEM_BALM_MUSHROOM,
+        ITEM_PEARL, ITEM_BIG_PEARL, ITEM_PEARL_STRING,
+        ITEM_STARDUST, ITEM_STAR_PIECE, ITEM_COMET_SHARD, ITEM_BIG_NUGGET,
+        ITEM_HEART_SCALE, ITEM_SHOAL_SALT, ITEM_SHOAL_SHELL,
+        ITEM_RED_SHARD, ITEM_BLUE_SHARD, ITEM_YELLOW_SHARD, ITEM_GREEN_SHARD,
+        ITEM_RELIC_COPPER, ITEM_RELIC_SILVER, ITEM_RELIC_GOLD,
+        ITEM_RELIC_VASE, ITEM_RELIC_BAND, ITEM_RELIC_STATUE, ITEM_RELIC_CROWN,
+        ITEM_PRETTY_FEATHER, ITEM_HONEY,
+        ITEM_REPEL, ITEM_SUPER_REPEL, ITEM_MAX_REPEL,
+        ITEM_ESCAPE_ROPE, ITEM_POKE_DOLL, ITEM_FLUFFY_TAIL,
     };
     const u8 *tid = gSaveBlock2Ptr->playerTrainerId;
     u32 seed = (u32)tid[0] | ((u32)tid[1] << 8) | ((u32)tid[2] << 16) | ((u32)tid[3] << 24);
