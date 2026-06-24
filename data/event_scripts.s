a72ac9c8f1ffc1c792f56b4d8c1d9cadf229e396
FFZackFair92 <ashsolo92@gmail.com>
2026-06-24 11:33:42 +0200

    Guide Deoxys: script + testo per ogni citta' Hoenn+Kanto (NvGuide_*), indicazioni obiettivo+prossima tappa secondo l'ordine linearizzato. Oldale gia' coperta. Manca il piazzamento object event.

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1166,6 +1166,7 @@ EventScript_SetBrineyLocation_Route109::
 	.include "data/scripts/pc.inc"
 	.include "data/scripts/move_relearner.inc"
 	.include "data/scripts/nuzverse_altars.inc"
+	.include "data/scripts/nuzverse_guides.inc"
 
 @ scripts/notices.inc? signs.inc? See comment about text/notices.inc
 Common_EventScript_ShowPokemartSign::
